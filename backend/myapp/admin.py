652e297d018ec152448a5278935ba519366634bf
DQD1K <125642042+DQD1K@users.noreply.github.com>
2023-10-04 20:21:47 +0300

    Delete backend directory

--- backend/myapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import TodoItem
-from .models import freelanceOrder
-
-# Register your models here.
-admin.site.register(TodoItem)
-admin.site.register(freelanceOrder)
