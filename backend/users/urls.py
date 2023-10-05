d202d942877cb67d0fd661ba2e4e27416439845f
voldezhur <voldezhur@gmail.com>
2023-10-05 21:41:25 +0300

    added logging out and registration

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("loginUser", views.loginUser, name="loginUser"),
+    path("logoutUser", views.logoutUser, name='logoutUser'),
+    path("registerUser", views.registerUser, name='registerUser')
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def loginUser(request):
@@ -12,8 +13,28 @@ def loginUser(request):
             login(request, user)
             return redirect("home")
         else:
-            messages.success(request, ("There was an error logging in, try again"))
+            messages.success(request, ("Ошибка со входом. Возможно указан неверный пароль"))
             return redirect("loginUser")
 
     else:
         return render(request, "authentication/login.html", {})
+    
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
+def registerUser(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+    
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'authentication/registerUser.html', {'form':form})
